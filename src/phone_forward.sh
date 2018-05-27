d091d0cf924d9107072e2587ce525fc7232d41eb
Michał Chojnowski <mc394134@students.mimuw.edu.pl>
2018-05-27 18:35:42 +0200

    Dodanie weryfikacji poprawności danych w skrypcie.

--- src/phone_forward.sh
+++ src/phone_forward.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 temp_in=$(mktemp)
 revs=$(mktemp)
-cat - "$2" <<<"NEW A" >> "$temp_in"
-cat "$temp_in" - <<<"?$3" | $1 >"$revs"
-sed "s/$/?/" "$revs" | cat "$temp_in" - | "$1" | paste -d' ' "$revs" - | grep " $3$" | cut -f1 -d' '
+if cat - "$2" <<<"NEW A" | "$1" &>/dev/null || grep -q "NEW\|DEL\|?" "$2"; then
+    echo "Bad redirection file."
+else
+    cat - "$2" <<<"NEW A" > "$temp_in"
+    cat "$temp_in" - <<<"?$3" | $1 >"$revs"
+    sed "s/$/?/" "$revs" | cat "$temp_in" - | "$1" | paste -d' ' "$revs" - | grep " $3$" | cut -f1 -d' '
+fi
 rm "$temp_in" "$revs"
