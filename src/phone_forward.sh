a7ce2a3ebc2da4a49e01e9310c932370bb2fe906
Michał Chojnowski <mc394134@students.mimuw.edu.pl>
2018-05-28 23:11:40 +0200

    Rozbudowanie weryfikacji błędów w skrypcie.

--- src/phone_forward.sh
+++ src/phone_forward.sh
@@ -5,18 +5,26 @@ output=$(mktemp)
 error_log=$(mktemp)
 
 cat - "$2" <<<"NEW A" > "$temp_in"
-if ! "$1" <"$temp_in" &>/dev/null || grep -q "NEW\|DEL\|?" "$2"; then
-    echo "Error while parsing input file."
-    rm "$temp_in"
+if ! "$1" <"$temp_in" >/dev/null 2>"$error_log"; then
+    echo -n "Error while parsing input file: " 1>&2
+    cat "$error_log" 1>&2
+    rm "$temp_in" "$error_log"
     exit 1
-else
-    cat "$temp_in" - <<<"?$3" | $1 >"$revs" 2>"$error_log"
-    sed "s/$/?/" "$revs" | cat "$temp_in" - | "$1" 2>"$error_log" | paste "$revs" - | grep "\s$3$" | cut -f1 >$output
-    if [ -s "$error_log" ]; then
-        echo "Execution error in $3."
-        rm "$temp_in" "$revs" "$output" "$error_log"
-        exit 1
-    fi
+fi
+if grep -o -m1 "NEW\|DEL\|?" "$2" >"$error_log"; then
+    echo -n "Illegal token in input file: " 1>&2
+    cat "$error_log" 1>&2
+    rm "$temp_in" "$error_log"
+    exit 1
+fi
+cat "$temp_in" - <<<"?$3" | $1 >"$revs" 2>"$error_log"
+sed "s/$/?/" "$revs" | cat "$temp_in" - | "$1" 2>"$error_log" |
+    paste "$revs" - | grep "\s$3$" | cut -f1 >"$output"
+if [ -s "$error_log" ]; then
+    echo -n "Runtime error in $3: " 1>&2
+    cat "$error_log" 1>&2
+    rm "$temp_in" "$revs" "$output" "$error_log"
+    exit 1
 fi
 cat "$output"
 rm "$temp_in" "$revs" "$output" "$error_log"
