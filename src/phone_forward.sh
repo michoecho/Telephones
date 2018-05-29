a144db050693496cd8e4e8d0e612ef7c87f6fb1a
Michał Chojnowski <mc394134@students.mimuw.edu.pl>
2018-05-29 14:45:52 +0200

    Nowa wersja skryptu.
    Nowa wersja skryptu przyjmuje jako plik z danymi także programy w języku
    phone_forward. Ponadto weryfikuje istnienie argumentów.

--- src/phone_forward.sh
+++ src/phone_forward.sh
@@ -1,30 +1,38 @@
 #/bin/bash
+# Skrypt przyjmuje jako poprawny argument $2 dowolny tekst,
+# który po dodaniu na początku linii "NEW A" jest poprawnym programem w języku
+# phone_forward, który po wykonaniu wszystkich instrukcji posiada obecną bazę.
+# Szukanie odwrotności numeru jest wykonywane na tej właśnie bazie.
 temp_in=$(mktemp)
 revs=$(mktemp)
 output=$(mktemp)
+temp_output=$(mktemp)
 error_log=$(mktemp)
 
-cat - "$2" <<<"NEW A" > "$temp_in"
-if ! "$1" <"$temp_in" >/dev/null 2>"$error_log"; then
+if ! [ -x $1 ]; then echo "Program '$1' not found."; exit 1; fi
+if ! [ -r $2 ]; then echo "Input file '$2' unavailable."; exit 1; fi
+if ! [[ "$3" =~ ^[0-9]+$ ]]; then echo "'$3' is not a number."; exit 1; fi
+
+cat - "$2" <<<"NEW A" >"$temp_in"
+if ! "$1" <"$temp_in" >"$temp_output" 2>"$error_log"; then
     echo -n "Error while parsing input file: " 1>&2
     cat "$error_log" 1>&2
-    rm "$temp_in" "$error_log"
+    rm "$temp_in" "$error_log" "$temp_output"
     exit 1
 fi
-if grep -o -m1 "NEW\|DEL\|?" "$2" >"$error_log"; then
-    echo -n "Illegal token in input file: " 1>&2
-    cat "$error_log" 1>&2
-    rm "$temp_in" "$error_log"
-    exit 1
-fi
-cat "$temp_in" - <<<"?$3" | $1 >"$revs" 2>"$error_log"
+unneeded=$(wc -l <"$temp_output")
+((++unneeded))
+rm "$temp_output"
+cat "$temp_in" - <<<"?$3" | $1 2>"$error_log" | tail -n -$unneeded >"$revs"
 sed "s/$/?/" "$revs" | cat "$temp_in" - | "$1" 2>"$error_log" |
-    paste "$revs" - | grep "\s$3$" | cut -f1 >"$output"
+    tail -n -$unneeded | paste "$revs" - | grep "\s$3$" | cut -f1 >"$output"
+
 if [ -s "$error_log" ]; then
-    echo -n "Runtime error in $3: " 1>&2
+    echo -n "Runtime error: " 1>&2
     cat "$error_log" 1>&2
     rm "$temp_in" "$revs" "$output" "$error_log"
     exit 1
 fi
+
 cat "$output"
 rm "$temp_in" "$revs" "$output" "$error_log"
